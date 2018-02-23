c8ec8f32e31333f2c43c7da2e4a7560319aaf058
andrewglevy <andrewglevy@gmail.com>
2018-02-23 09:19:54 -0800

    move variable declarations into promise scope

--- main.js
+++ main.js
@@ -1,8 +1,6 @@
 
 document.getElementById('postForm').addEventListener('submit', function(e){
 	let formText = document.getElementById('formText').value;
-	let s3;
-	let params;
 
 	axios.post('https://2gi7ndbzoe.execute-api.us-west-2.amazonaws.com/dev/get-key', JSON.stringify({
 		"data": formText})
@@ -13,14 +11,14 @@ document.getElementById('postForm').addEventListener('submit', function(e){
 	})
 	.then(function(result){
 		console.log(result[0], result[1])
-		s3 = new AWS.S3();
-        params = {
+		let s3 = new AWS.S3();
+        let params = {
 			Bucket: result[0],
 			Key: result[1],
 			Body: formText
 		}
 		return s3.upload(params).promise()
-		.then (function(result){
+		.then(function(result){
 			return result;
 		});
 
@@ -30,34 +28,3 @@ document.getElementById('postForm').addEventListener('submit', function(e){
 	});
 	e.preventDefault();
 });
-// function performPostRequest(e) {
-// 	let formText = document.getElementById('formText').value;
-// 	let s3;
-// 	let params;
-
-// 	axios.post('https://2gi7ndbzoe.execute-api.us-west-2.amazonaws.com/dev/get-key', JSON.stringify({
-// 		"data": formText})
-// 	)
-// 	.then(function(response){
-// 		let data = response.data.data;
-// 		return [data.bucket, data.Key];
-// 	})
-// 	.then(function(result){
-// 		console.log(result[0], result[1])
-// 		s3 = new AWS.S3();
-//         params = {
-// 			Bucket: result[0],
-// 			Key: result[1],
-// 			Body: formText
-// 		}
-// 		return s3.upload(params).promise()
-// 		.then (function(result){
-// 			return result;
-// 		});
-
-// 	})
-// 	.catch(function(error){
-// 		console.log(error);
-// 	});
-// 	e.preventDefault();
-// }
