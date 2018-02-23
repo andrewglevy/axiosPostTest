9a8845061db3dc3c583e43d8c06e2f45a45ddc05
andrewglevy <andrewglevy@gmail.com>
2018-02-22 17:43:27 -0800

    make function anonymous an called by addEventListener

--- main.js
+++ main.js
@@ -1,12 +1,8 @@
-let s3;
-let params;
 
-document.getElementById('postForm').addEventListener('submit', performPostRequest)
-
-function performPostRequest(e) {
-	let result = document.getElementById('result');
+document.getElementById('postForm').addEventListener('submit', function(e){
 	let formText = document.getElementById('formText').value;
-	result.innerHTML = '';
+	let s3;
+	let params;
 
 	axios.post('https://2gi7ndbzoe.execute-api.us-west-2.amazonaws.com/dev/get-key', JSON.stringify({
 		"data": formText})
@@ -17,7 +13,7 @@ function performPostRequest(e) {
 	})
 	.then(function(result){
 		console.log(result[0], result[1])
-		s3 = new AWS.S3()
+		s3 = new AWS.S3();
         params = {
 			Bucket: result[0],
 			Key: result[1],
@@ -26,11 +22,42 @@ function performPostRequest(e) {
 		return s3.upload(params).promise()
 		.then (function(result){
 			return result;
-		})
+		});
 
 	})
 	.catch(function(error){
 		console.log(error);
-	})
+	});
 	e.preventDefault();
-}
+});
+// function performPostRequest(e) {
+// 	let formText = document.getElementById('formText').value;
+// 	let s3;
+// 	let params;
+
+// 	axios.post('https://2gi7ndbzoe.execute-api.us-west-2.amazonaws.com/dev/get-key', JSON.stringify({
+// 		"data": formText})
+// 	)
+// 	.then(function(response){
+// 		let data = response.data.data;
+// 		return [data.bucket, data.Key];
+// 	})
+// 	.then(function(result){
+// 		console.log(result[0], result[1])
+// 		s3 = new AWS.S3();
+//         params = {
+// 			Bucket: result[0],
+// 			Key: result[1],
+// 			Body: formText
+// 		}
+// 		return s3.upload(params).promise()
+// 		.then (function(result){
+// 			return result;
+// 		});
+
+// 	})
+// 	.catch(function(error){
+// 		console.log(error);
+// 	});
+// 	e.preventDefault();
+// }
