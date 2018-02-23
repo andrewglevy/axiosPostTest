02bc15fc576e0ce85688d6736a1994903b56b55d
andrewglevy <andrewglevy@gmail.com>
2018-02-22 17:10:59 -0800

    add new s3 object and params to promise chain of performPostRequest

--- main.js
+++ main.js
@@ -1,27 +1,22 @@
+// function generateSuccessHTMLOutput(response){
+// 	data = response.data.data;
+// 	console.log(data);
+// 	key = data.Key;
+// 	console.log(key);
+// 	bucket = data.bucket;
+// 	return '<h4>Result: </h4> <p>' + 'key: ' + key + '<br />' +
+// 	'bucket: ' + bucket + '</p>';
+// }
+// function generateErrorHTMLOutput(error){
+// 	err = error.response;
+// 	console.log('error: ', error)
+// 	return '<h4>Result:>/h4> <p>' + err + '<p>';
+// }
 
-let data;
-let key;
-let bucket;
-let err;
-
-
-function generateSuccessHTMLOutput(response){
-	data = response.data.data;
-	console.log(data);
-	key = data.Key;
-	console.log(key);
-	bucket = data.bucket;
-	return '<h4>Result: </h4> <p>' + 'key: ' + key + '<br />' +
-	'bucket: ' + bucket + '</p>';
-}
-function generateErrorHTMLOutput(error){
-	err = error.response;
-	console.log('error: ', error)
-	return '<h4>Result:>/h4> <p>' + err + '<p>';
-}
+let s3;
+let params;
 
 document.getElementById('postForm').addEventListener('submit', performPostRequest)
-
 
 function performPostRequest(e) {
 	let result = document.getElementById('result');
@@ -32,7 +27,19 @@ function performPostRequest(e) {
 		"data": formText})
 	)
 	.then(function(response){
-		result.innerHTML = generateSuccessHTMLOutput(response);
+		let data = response.data.data;
+		return [data.bucket, data.Key];
+	})
+	.then(function(result){
+		console.log(result[0], result[1])
+		s3 = new AWS.S3()
+        params = {
+			Bucket: result[0],
+			Key: result[1],
+			Body: formText
+		}
+
+
 	})
 	.catch(function(error){
 		generateErrorHTMLOutput(error);
