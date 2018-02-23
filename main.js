bb24d69f26ef531a4b710a659e4384e65f4426ca
andrewglevy <andrewglevy@gmail.com>
2018-02-23 11:05:24 -0800

    include aws.config.update and s3 instance w/ access/secret keys and s3 info. code does not currently work

--- main.js
+++ main.js
@@ -1,18 +1,41 @@
 
-document.getElementById('postForm').addEventListener('submit', function(e){
-	let formText = document.getElementById('formText').value;
+AWS.config.update({
+    accessKeyId: "KEY GOES HERE",
+    secretAccessKey: "KEY GOES HERE",
+    bucket : "s3-post-test",
+    region: "us-west-2"
+});
+
+document.getElementById('postForm').addEventListener('submit', function(event){
+	const formText = document.getElementById('formText').value;
 
 	axios.post('https://2gi7ndbzoe.execute-api.us-west-2.amazonaws.com/dev/get-key', JSON.stringify({
-		"data": formText})
+		"data": formText}, {
+			headers: {
+				'Access-Control-Allow-Credentials': true,
+				'Access-Control-Allow-Origin': '*',
+				'Content-Type': 'application/json'
+			}
+		})
 	)
 	.then(function(response){
-		let data = response.data.data;
+		const data = response.data.data;
 		return [data.bucket, data.Key];
 	})
 	.then(function(result){
 		console.log(result[0], result[1])
-		let s3 = new AWS.S3();
-        let params = {
+		const s3 = new AWS.S3({
+			apiVersion: '2006-03-01',
+			accessKeyId: "KEY GOES HERE",
+			secretAccessKey: "KEY GOES HERE",
+			region: "us-west-2",
+			params: {
+				Bucket: result[0],
+				Key: result[1],
+				Body: formText
+			}
+		});
+        const params = {
 			Bucket: result[0],
 			Key: result[1],
 			Body: formText
@@ -26,5 +49,5 @@ document.getElementById('postForm').addEventListener('submit', function(e){
 	.catch(function(error){
 		console.log(error);
 	});
-	e.preventDefault();
+	event.preventDefault();
 });
