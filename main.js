dea330f8aaa9bf39341abfa51b35038e0ea521d2
andrewglevy <andrewglevy@gmail.com>
2018-02-26 16:13:03 -0800

    fix file upload via setting content type to octet stream and parsing the formData object correctly

--- main.js
+++ main.js
@@ -1,21 +1,30 @@
 
+const form = document.getElementById('postForm');
+form.addEventListener('submit', function(event){
+	event.preventDefault();
+	let formdata = new FormData(event.target);
+	console.log(formdata);
 
-document.getElementById('postForm').addEventListener('submit', function(event){
-	const formText = document.getElementById('formText').value;
-	const result = document.getElementById('x');
+	const result = document.getElementById('result');
 	result.innerHTML = '';
 
-	axios.post('https://2gi7ndbzoe.execute-api.us-west-2.amazonaws.com/dev/get-key', JSON.stringify({"data": formText}))
-	.then(function(response){
+	axios.post('https://2gi7ndbzoe.execute-api.us-west-2.amazonaws.com/dev/get-key', JSON.stringify({"data": "data"}))
+	.then(function(response) {
 		const url = response.data.url;
 		console.log(url);
-		result.innerHTML = '<h3>URL: </h3><p style="word-wrap:break-word">' + url + '</p>';
+		result.innerHTML = '<h3">URL: </h3><p style="word-wrap:break-word">' + url + '</p>';
 		return url;
+	})
+	.then(function(url) {
+
+		return axios.put(url, formdata.get('file'), {
+			headers: {
+				'content-type': 'application/octet-stream'
+			}
+		});
 	})
 	.catch(function(error){
 		console.log(error);
-		result.innerHTML = '<p>An Error Occurred. See console for details.</p>';
+		result.innerHTML = 'An error occurred.';
 	});
-
-	event.preventDefault();
 });
