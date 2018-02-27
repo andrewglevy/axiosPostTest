5b4c0c5e93e3f00a6603275348727c7ad1d91053
andrewglevy <andrewglevy@gmail.com>
2018-02-26 16:20:30 -0800

    clean up code

--- main.js
+++ main.js
@@ -1,30 +1,23 @@
-
 const form = document.getElementById('postForm');
 form.addEventListener('submit', function(event){
+	const formdata = new FormData(event.target);
+	const file = formdata.get('file');
 	event.preventDefault();
-	let formdata = new FormData(event.target);
-	console.log(formdata);
-
-	const result = document.getElementById('result');
-	result.innerHTML = '';
-
-	axios.post('https://2gi7ndbzoe.execute-api.us-west-2.amazonaws.com/dev/get-key', JSON.stringify({"data": "data"}))
+	axios.post('https://2gi7ndbzoe.execute-api.us-west-2.amazonaws.com/dev/get-key', JSON.stringify({'file': file}))
 	.then(function(response) {
-		const url = response.data.url;
-		console.log(url);
-		result.innerHTML = '<h3">URL: </h3><p style="word-wrap:break-word">' + url + '</p>';
-		return url;
+		return response.data.url;
 	})
 	.then(function(url) {
-
-		return axios.put(url, formdata.get('file'), {
+		return axios.put(url, file, {
 			headers: {
 				'content-type': 'application/octet-stream'
 			}
 		});
 	})
 	.catch(function(error){
-		console.log(error);
+		const result = document.getElementById('result');
+		result.innerHTML = '';
 		result.innerHTML = 'An error occurred.';
+		console.log(error);
 	});
 });
